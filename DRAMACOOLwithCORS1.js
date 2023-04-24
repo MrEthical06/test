a003a50e6d17b28cbacf440ba57c6b7029e0dd17
toonpedia <70449705+toonpedia@users.noreply.github.com>
2023-04-24 10:30:53 +0530

    Update DRAMACOOLwithCORS1.js

--- DRAMACOOLwithCORS1.js
+++ DRAMACOOLwithCORS1.js
@@ -1,12 +1,21 @@
 //import { uri } from "./uri.js";
 
-const uri = "https://api.consumet.org/";
-const epnum = 1; 
+
+//following are to be changed as per source
+const uri = "https://animia.xyz/";
+const showinfo = "movies/dramacool/info?id=";
+const epinfo = "movies/dramacool/watch?episodeId=";
+
+
+//following are to be changed as per show
+const epnum = 0; 
+const showid = "drama-detail/emergency-lands-of-love";
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
 
-	fetch(`${uri}`+ "movies/dramacool/info?id=drama-detail/emergency-lands-of-love")
+	fetch(`${uri}`+ `${showinfo}` + `${showid}`)
 		.then(response => {
 			if (!response.ok) {
 				throw Error("Error");
@@ -16,35 +25,20 @@ document.addEventListener('DOMContentLoaded', () => {
 		})
 
 		.then(data => {
+			/// use forEach here to get id of every episodes and then use following most of the code (which is required for one player) for every player. then initiate plyr refer https://codepen.io/jackjona/pen/JjypzWj
 
 
-
-			//const files = data.episodes.map(episodes => {
-
-				var epId = response.episodes[`${epnum}`].id
 				var epId = data.episodes[`${epnum}`].id
 				var mediaId = data.id
 
 
-				fetch(`${uri}`+"movies/dramacool/watch?episodeId=" + epId + "&mediaId=" + mediaId)
+				fetch(`${uri}`+ `${epinfo}` + epId + "&mediaId=" + mediaId)
 					.then(response => {
 						if (!response.ok) {
 							throw Error("Error");
 						}
 						return response.json();
 					})
-
-					///////////////////////////////////////////////////////////////////////////////////
-					//.then(data => {
-
-					//const files = data.sources.map(sources => {
-					//var ep = `${sources.url}`;
-					//var ep = response.sources[3].url
-					///////////////////////////////////////////////////////////////////////////////////////
-					//the following changes (originally it was as in //..) will find auto quality m3u8 file i.e. playlist.m3u8 for tmdb just change this syntax a little for getting playlists in other api.
-					//if there is only one src then use above {var ep} part
-					// primarily will have to change quality, sources, url for src
-
 
 					.then(response => {
 
@@ -55,7 +49,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 						var playlistsource = allsources.find(istrue)
 						var url = playlistsource.url
-						console.log(url)
 
 
 						//following will always fins url for english subtitles if inside array lang is given and lang=english
@@ -65,21 +58,6 @@ document.addEventListener('DOMContentLoaded', () => {
 						var langforsub = response.subtitles[0].lang
 
 
-						////////////////////////////////////////////////////////////////////////////////////////////////////
-						//following settings can be used when we need specific subtitle at specific location
-						//var subt = response.subtitles[7].url
-						//var id = allsubs.indexOf("url");
-						//console.log(id)
-
-						//const subt = data.subtitles.map(subtitles => {
-						//	var subUrl = `${subtitles.url}`;
-						//	var subLang = `${subtitles.lang}`;
-
-						////////////////////////////////////////////////////////////////////////////////////////////////
-						//const source = "https://cors.consumet.stream/${ep}";
-
-
-						//const source = `${ep}`
 
 						const source = 'https://proxy.vnxservers.com/proxy/m3u8/' + encodeURIComponent(url)
 						const video = document.querySelector('video');
@@ -108,29 +86,16 @@ document.addEventListener('DOMContentLoaded', () => {
 							const hls = new Hls();
 							hls.loadSource(source);
 
-							// From the m3u8 playlist, hls parses the manifest and returns
-							// all available video qualities. This is important, in this approach,
-							// we will have one source on the Plyr player.
-
 							hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
 
-								// Transform available levels into an array of integers (height values).
-
 								const availableQualities = hls.levels.map((l) => l.height)
-								availableQualities.unshift(0) //prepend 0 to quality array
-
-								// Add new qualities to option
-								//these quality option are for tmdb. change default:2 & 2:auto as per requirement. prefer 0 if or auto in fetched (links) streaming qualities
-								//the above statement is no more needed as above we directly find source with m3u8 playlist and load it as url
-
+								availableQualities.unshift(0) 
 								defaultOptions.quality = {
 									default: 0, //Default - AUTO
 									options: availableQualities,
 									forced: true,
 									onChange: (e) => updateQuality(e),
 								}
-
-								// Add Auto Label
 
 								defaultOptions.i18n = {
 									qualityLabel: {
@@ -166,21 +131,6 @@ document.addEventListener('DOMContentLoaded', () => {
 								});
 							}
 						}
-
-						//})
-						//})
 					});
-			//})
-			//})
-
 		})
 })
-
-
-
-
-
-
-
-
-
