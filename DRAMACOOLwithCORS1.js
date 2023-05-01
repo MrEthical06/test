45701ddb8c402dee5a601c28ddccd0996d2ec07c
toonpedia <70449705+toonpedia@users.noreply.github.com>
2023-05-01 22:23:02 +0530

    Update DRAMACOOLwithCORS1.js

--- DRAMACOOLwithCORS1.js
+++ DRAMACOOLwithCORS1.js
@@ -1,136 +1,133 @@
-//import { uri } from "./uri.js";
 
-
-//following are to be changed as per source
-const uri = "https://animia.xyz/";
-const showinfo = "movies/dramacool/info?id=";
-const epinfo = "movies/dramacool/watch?episodeId=";
-
+const uri = "https://api.consumet.org/";
+const epinfo = "meta/tmdb/watch/";
 
 //following are to be changed as per show
-const epnum = 0; 
-const showid = "drama-detail/emergency-lands-of-love";
+const epnum = 0;
 
+fetch("https://api.consumet.org/meta/tmdb/info/94796?type=tv")
+	.then(response => {
+		if (!response.ok) {
+			throw Error("Error");
+		}
+		return response.json();
 
-document.addEventListener('DOMContentLoaded', () => {
+	})
 
+	.then(data => {
 
-	fetch(`${uri}`+ `${showinfo}` + `${showid}`)
-		.then(response => {
-			if (!response.ok) {
-				throw Error("Error");
-			}
-			return response.json();
-
-		})
-
-		.then(data => {
-			/// use forEach here to get id of every episodes and then use following most of the code (which is required for one player) for every player. then initiate plyr refer https://codepen.io/jackjona/pen/JjypzWj
-
-
-				var epId = data.episodes[`${epnum}`].id
-				var mediaId = data.id
-
-
-				fetch(`${uri}`+ `${epinfo}` + epId + "&mediaId=" + mediaId)
-					.then(response => {
-						if (!response.ok) {
-							throw Error("Error");
-						}
-						return response.json();
-					})
-
-					.then(response => {
-
-						var allsources = response.sources
-						function istrue(sources) {
-							return sources.isM3U8 === true;
-						}
-
-						var playlistsource = allsources.find(istrue)
-						var url = playlistsource.url
-
-
-						//following will always fins url for english subtitles if inside array lang is given and lang=english
-
-						var urlforsub = response.subtitles[0].url
-
-						var langforsub = response.subtitles[0].lang
+		const files = data.seasons.map(seasons => {
+			var allep = seasons.episodes;
+			var epId = allep[`${epnum}`].id
+			var mediaId = data.id
 
 
 
-						const source = 'https://proxy.vnxservers.com/proxy/m3u8/' + encodeURIComponent(url)
-						const video = document.querySelector('video');
+			fetch(`${uri}` + `${epinfo}` + epId + "?id=" + mediaId)
+				.then(response => {
+					if (!response.ok) {
+						throw Error("Error");
+					}
+					return response.json();
+				})
 
-						const defaultOptions = {
-							captions: { active: true, update: true, language: 'en', src: `${urlforsub}` }
-						};
 
+				.then(response => {
+
+					var allsources = response.sources
+					function istrue(sources) {
+						return sources.isM3U8 === true;
+					}
+
+					var playlistsource = allsources.find(istrue)
+					var url = playlistsource.url
+
+					const source = `${url}`;
+					const video = document.querySelector('video');
+
+
+					////////////////////////////////////////////////////////////////
+					var allsubs = response.subtitles
+
+
+
+
+					for (var i = 0; i < allsubs.length; i++) {
 						const track = document.createElement('track');
 						Object.assign(track, {
-							label: 'English',
-							srclang: 'en',
-							default: true,
-							src: 'https://proxy.vnxservers.com/proxy/m3u8/' + encodeURIComponent(urlforsub)
+							label: `${allsubs[i].lang}`,
+							default: false,
+							src: `${proxy}` + encodeURIComponent(`${allsubs[i].url}`)
 						});
 						video.appendChild(track);
+					}
 
 
-						if (!Hls.isSupported()) {
-							video.src = source;
-							var player = new Plyr(video, defaultOptions);
-						} else {
 
-							// For more Hls.js options, see https://github.com/dailymotion/hls.js
 
-							const hls = new Hls();
-							hls.loadSource(source);
+					//////////////////////////////////////////////////////////////////////////////////////////////////////
 
-							hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+					const defaultOptions = {
+						captions: { active: true, update: true }
+					};
 
-								const availableQualities = hls.levels.map((l) => l.height)
-								availableQualities.unshift(0) 
-								defaultOptions.quality = {
-									default: 0, //Default - AUTO
-									options: availableQualities,
-									forced: true,
-									onChange: (e) => updateQuality(e),
+					if (!Hls.isSupported()) {
+						video.src = source;
+						var player = new Plyr(video, defaultOptions);
+					} else {
+
+						const hls = new Hls();
+						hls.loadSource(source);
+
+						hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+
+							const availableQualities = hls.levels.map((l) => l.height)
+							availableQualities.unshift(0) 
+							defaultOptions.quality = {
+								default: 0, //Default - AUTO
+								options: availableQualities,
+								forced: true,
+								onChange: (e) => updateQuality(e),
+							}
+
+							// Add Auto Label
+
+							defaultOptions.i18n = {
+								qualityLabel: {
+									0: 'Auto',
+								},
+							}
+
+							hls.on(Hls.Events.LEVEL_SWITCHED, function (event, data) {
+								var span = document.querySelector(".plyr__menu__container [data-plyr='quality'][value='0'] span")
+								if (hls.autoLevelEnabled) {
+									span.innerHTML = `AUTO (${hls.levels[data.level].height}p)`
+								} else {
+									span.innerHTML = `AUTO`
 								}
-
-								defaultOptions.i18n = {
-									qualityLabel: {
-										0: 'Auto',
-									},
-								}
-
-								hls.on(Hls.Events.LEVEL_SWITCHED, function (event, data) {
-									var span = document.querySelector(".plyr__menu__container [data-plyr='quality'][value='0'] span")
-									if (hls.autoLevelEnabled) {
-										span.innerHTML = `AUTO (${hls.levels[data.level].height}p)`
-									} else {
-										span.innerHTML = `AUTO`
-									}
-								});
-
-								var player = new Plyr(video, defaultOptions);
 							});
 
-							hls.attachMedia(video);
-							window.hls = hls;
-						}
+							var player = new Plyr(video, defaultOptions);
+						});
 
-						function updateQuality(newQuality) {
-							if (newQuality === 0) {
-								window.hls.currentLevel = -1; //Enable AUTO quality if option.value = 0
-							} else {
-								window.hls.levels.forEach((level, levelIndex) => {
-									if (level.height === newQuality) {
-										console.log("Found quality match with " + newQuality);
-										window.hls.currentLevel = levelIndex;
-									}
-								});
-							}
+						hls.attachMedia(video);
+						window.hls = hls;
+					}
+
+					function updateQuality(newQuality) {
+						if (newQuality === 0) {
+							window.hls.currentLevel = -1; //Enable AUTO quality if option.value = 0
+						} else {
+							window.hls.levels.forEach((level, levelIndex) => {
+								if (level.height === newQuality) {
+									console.log("Found quality match with " + newQuality);
+									window.hls.currentLevel = levelIndex;
+								}
+							});
 						}
-					});
+					}
+
+				});
 		})
-})
+	})
+
